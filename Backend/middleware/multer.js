379b9cee2abc05c8bcb71459a23156b4107837eb
Muhammad Sameer <136972597+Sameer-khan0@users.noreply.github.com>
2024-03-08 15:00:18 +0500

    test

--- Backend/middleware/multer.js
+++ Backend/middleware/multer.js
@@ -3,7 +3,11 @@ const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    const uploadDir = "temp/uploads/";
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir);
+    }
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + "-" + file.originalname);
