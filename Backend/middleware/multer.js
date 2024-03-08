147c1f02946d5a8afa0b89c0b103d2eb9f475286
Muhammad Sameer <136972597+Sameer-khan0@users.noreply.github.com>
2024-03-08 15:16:21 +0500

    testin..

--- Backend/middleware/multer.js
+++ Backend/middleware/multer.js
@@ -1,28 +1,18 @@
-const multer = require("multer");
-const fs = require("fs");
+const multer=require('multer')
+const fs=require('fs')
 
-const storage = multer.diskStorage({
+const storage=multer.diskStorage({
     destination: function (req, file, cb) {
-      const uploadDir = "../temp";
-      if (!fs.existsSync(uploadDir)) {
-        fs.mkdirSync(uploadDir, { recursive: true });
+        const uploadDir = 'temp/';
+        // if (!fs.existsSync(uploadDir)) {
+        //   fs.mkdirSync(uploadDir);
+        // }
+        cb(null, uploadDir);
+      } ,
+      filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
       }
-      cb(null, uploadDir);
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now() + "-" + file.originalname);
-    }
-  // destination: function (req, file, cb) {
-  //     const uploadDir = "./uploads";
-  //     if (!fs.existsSync(uploadDir)) {
-  //       fs.mkdirSync(uploadDir);
-  //     }
-  //     cb(null, uploadDir);
-  //   } ,
-  //   filename: function (req, file, cb) {
-  //     cb(null, Date.now() + '-' + file.originalname);
-  //   }
-});
+})
 
-const upload = multer({ storage: storage });
-module.exports = upload;
+const upload=multer({storage:storage})
+module.exports=upload
